e2eddce873ee26b073b54ac9d62965384a34c260
apthagowda97 <iamuraptha@gmail.com>
2020-07-11 21:48:25 +0530

    added parser to the main

--- src/main.py
+++ src/main.py
@@ -10,48 +10,96 @@ from landmark_regression import LandmarkRegression
 from head_pose import HeadPose
 from gaze_estimation import GazeEstimation
 
-import matplotlib.pyplot as plt
+from argparse import ArgumentParser
 
-DIR_PATH = os.path.split(os.getcwd())[0]
-video_file = os.path.join(DIR_PATH,"bin\\demo.mp4")
+def build_argparser():
+    """
+    Parse command line arguments.
 
-face_model = os.path.join(DIR_PATH,os.path.join('model\\intel\\face-detection-adas-binary-0001\\FP32-INT1\\face-detection-adas-binary-0001'))
-landmark_model= os.path.join(DIR_PATH,os.path.join('model\\intel\\landmarks-regression-retail-0009\\FP32-INT8\\landmarks-regression-retail-0009'))
-headpose_model= os.path.join(DIR_PATH,os.path.join('model\\intel\\head-pose-estimation-adas-0001\\FP32-INT8\\head-pose-estimation-adas-0001'))
-gaze_model = os.path.join(DIR_PATH,os.path.join('model\\intel\\gaze-estimation-adas-0002\\FP32-INT8\\gaze-estimation-adas-0002'))
+    :return: command line arguments
+    """
+    parser = ArgumentParser()
 
-face_detection= FaceDetection(face_model)
-face_detection.load_model()
+    parser.add_argument("-f", "--face_model", required=True, type=str,
+                        help="Path to an xml file with a trained model.")
+    parser.add_argument("-he", "--head_model", required=True, type=str,
+                        help="Path to an xml file with a trained model.")
+    parser.add_argument("-l", "--landmark_model", required=True, type=str,
+                        help="Path to an xml file with a trained model.")
+    parser.add_argument("-g", "--gaze_model", required=True, type=str,
+                        help="Path to an xml file with a trained model.")
+    parser.add_argument("-t", "--input_type", required=True, type=str,default="cam",
+                        help="'video', 'cam' or 'image' input type")
+    parser.add_argument("-i", "--input_file",type=str,default=None,
+                        help="Path to image or video file")
+    parser.add_argument("-d", "--device", type=str, default="CPU",
+                        help="Specify the target device to infer on")
+    return parser
 
-landmark_regression= LandmarkRegression(landmark_model)
-landmark_regression.load_model()
+def inference(args):
+    
+    time_sheet = {'model_load':[],'face_infr':[],'landmark_infr':[],'head_infr':[],'gaze_infr':[]}
 
-head_pose = HeadPose(headpose_model)
-head_pose.load_model()
+    model_load_start = time.time()
 
-gaze_estimation = GazeEstimation(gaze_model)
-gaze_estimation.load_model()
+    face_detection= FaceDetection(args.face_model)
+    face_detection.load_model()
+    landmark_regression= LandmarkRegression(args.landmark_model)
+    landmark_regression.load_model()
+    head_pose = HeadPose(args.head_model)
+    head_pose.load_model()
+    gaze_estimation = GazeEstimation(args.gaze_model)
+    gaze_estimation.load_model()
 
-mouse_controller = MouseController('medium','fast')
+    time_sheet['model_load'].append(time.time()-model_load_start)
 
-input_feeder = InputFeeder('cam',video_file)
-input_feeder.load_data()
+    mouse_controller = MouseController('medium','fast')
 
-cv2.namedWindow('preview', cv2.WND_PROP_FULLSCREEN)
-cv2.setWindowProperty('preview', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.namedWindow('preview', cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty('preview', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-start_time = time.time()
-for i in range(20):
-    image = next(iter(input_feeder.next_batch()))
-    face_image = face_detection.predict(image)
-    left_eye_image,right_eye_image = landmark_regression.predict(np.copy(face_image))
-    head_pose_angles = head_pose.predict(np.copy(face_image))
-    x,y,z = gaze_estimation.predict(left_eye_image,right_eye_image,head_pose_angles)
-    cv2.imshow('preview',image)
-    mouse_controller.move(x,y)
-    key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
-        break
-print("Infr time per frame: ",(time.time()-start_time)/20)
-cv2.destroyWindow("preview")
-input_feeder.close()
+    input_feeder = InputFeeder(args.input_type,args.input_file)
+    input_feeder.load_data()
+
+    for image in input_feeder.next_batch():
+
+        face_infr_start = time.time()
+        face_image = face_detection.predict(image)
+        time_sheet['face_infr'].append(time.time()-face_infr_start)
+
+        landmark_infr_start = time.time()
+        left_eye_image,right_eye_image = landmark_regression.predict(np.copy(face_image))
+        time_sheet['landmark_infr'].append(time.time()-landmark_infr_start)
+
+        head_infr_start = time.time()       
+        head_pose_angles = head_pose.predict(np.copy(face_image))
+        time_sheet['head_infr'].append(time.time()-head_infr_start)
+
+        gaze_infr_start = time.time()
+        x,y,z = gaze_estimation.predict(left_eye_image,right_eye_image,head_pose_angles)
+        time_sheet['gaze_infr'].append(time.time()-gaze_infr_start)
+
+        cv2.imshow('preview',image)
+        mouse_controller.move(x,y)
+        key = cv2.waitKey(20)
+        if key == 27: # exit on ESC
+            return time_sheet
+
+    input_feeder.close()
+    return time_sheet
+
+def main():
+    args = build_argparser().parse_args()
+    time_sheet = inference(args)
+    print("Model load time: ",np.mean(time_sheet['model_load']))
+    print("Face infr time: ",np.mean(time_sheet['face_infr']))
+    print("landmark infr time: ",np.mean(time_sheet['landmark_infr']))
+    print("head infr time: ",np.mean(time_sheet['head_infr']))
+    print("gaze infr time: ",np.mean(time_sheet['gaze_infr']))
+
+
+
+if __name__ == '__main__':
+    main()
+
+
